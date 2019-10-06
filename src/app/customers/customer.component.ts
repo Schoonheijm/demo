630c93c0c54f1514b32fd8b7d764aedfd8ac8988
schoonheijm <j.r.schoonheijm@ziggo.nl>
2019-10-06 10:59:13 +0200

    initial commit

---
+++ src/app/customers/customer.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.css']
+})
+export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
+  customer = new Customer();
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: ['',[Validators.required, Validators.minLength(3)]],
+      lastName: ['',[Validators.required, Validators.minLength(50)]],
+      email: '',
+      sendCatalog: true
+    });
+  }
+
+  populateTestData(): void {
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      sendCatalog: false
+    });
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+}
